b199b421b8f0c4b51fded132acda99c1d70a1411
David Crawshaw <david@zentus.com>
2006-08-13 11:37:53 +1000

    test for correct response from RS.getObject()
    darcs-hash:20060813013753-0c629-96a01c3fcee4c2c677a823d9027c9f052a3d6df3.gz

--- src/test/Test12.java
+++ src/test/Test12.java
@@ -7,22 +7,30 @@ public class Test12 implements Test.Case
     private String error;
     private Exception ex;
 
+    private boolean same(byte[] a, byte[] b) {
+        if (a == null || b == null || a.length != b.length) return false;
+        for (int i=0; i < a.length; i++) if (a[i] != b[i]) return false;
+        return true;
+    }
+
     public boolean run() throws Exception {
         int res;
         byte[] origbytes = new byte[] {1,2,3, 1,2,3, 4,5,6, 4,5,6, 8,8,8};
 
         Class.forName("org.sqlite.JDBC");
-        Connection conn = DriverManager.getConnection("jdbc:sqlite::memory:");
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:");
 
         Statement stat = conn.createStatement();
-        stat.executeUpdate("CREATE TEMP TABLE Test12 (c1,c2,c3,c4,c5,c6,c7);");
+        stat.executeUpdate(
+            "create temp table test12 ("
+            + "c1 integer, c2 float, c3, c4 varchar, c5 bit, c6, c7);");
 
         PreparedStatement prep = conn.prepareStatement(
-            "INSERT INTO Test12 VALUES (?,?,?,?,?,?,?);");
+            "insert into test12 values (?,?,?,?,?,?,?);");
 
-        prep.setInt    (1, 99);
-        prep.setFloat  (2, 54.77F);
-        prep.setDouble (3, 43.44D);
+        prep.setInt    (1, Integer.MAX_VALUE);
+        prep.setFloat  (2, Float.MAX_VALUE);
+        prep.setDouble (3, Double.MAX_VALUE);
         prep.setLong   (4, Long.MAX_VALUE);
         prep.setBoolean(5, false);
         prep.setByte   (6, (byte)7);
@@ -32,20 +40,58 @@ public class Test12 implements Test.Case
         ResultSet rs = stat.executeQuery(
             "SELECT c1,c2,c3,c4,c5,c6,c7 FROM Test12;");
         if (!rs.next()) { error = "expected result"; return false; }
-        if (rs.getInt(1) != 99) { error = "bad int"; return false; }
-        if (rs.getFloat(2) != 54.77F) { error = "bad float"; return false; }
-        if (rs.getDouble(3) != 43.44D) { error = "bad double"; return false; }
-        if (rs.getLong(4) != Long.MAX_VALUE) { error="bad long"; return false; }
-        if (rs.getBoolean(5)) { error = "bad boolean"; return false; }
-        if (rs.getByte(6) != (byte)7) { error = "bad byte"; return false; }
-        byte[] getbytes = rs.getBytes(7);
-        if (origbytes.length != getbytes.length) {
-            error = "bad bytes length, got " + getbytes.length; return false;
+
+        if (rs.getInt(1) != Integer.MAX_VALUE) {
+            error = "bad int"; return false; }
+        if (rs.getFloat(2) != Float.MAX_VALUE) {
+            error = "bad float"; return false; }
+        if (rs.getDouble(3) != Double.MAX_VALUE) {
+            error = "bad double"; return false; }
+        if (rs.getLong(4) != Long.MAX_VALUE) {
+            error="bad long"; return false; }
+        if (rs.getBoolean(5)) {
+            error = "bad boolean"; return false; }
+        if (rs.getByte(6) != (byte)7) {
+            error = "bad byte"; return false; }
+        if (!same(rs.getBytes(7), origbytes)) {
+            error = "bad bytes"; return false; }
+
+        Object o;
+
+        o = rs.getObject(1);
+        if (o == null || !(o instanceof Integer)
+                || ((Integer)o).intValue() != Integer.MAX_VALUE) {
+            error = "bad object int"; return false;
         }
-        for (int i=0; i < origbytes.length; i++) {
-            if (origbytes[i] != getbytes[i]) {
-                error = "bad bytes["+i+"]"; return false;
-            }
+        o = rs.getObject(2);
+        if (o == null || !(o instanceof Double)
+                || ((Double)o).floatValue() != Float.MAX_VALUE) {
+            error = "bad object float"; return false;
+        }
+        o = rs.getObject(3);
+        if (o == null || !(o instanceof Double)
+                || ((Double)o).doubleValue() != Double.MAX_VALUE) {
+            error = "bad object double"; return false;
+        }
+        o = rs.getObject(4);
+        if (o == null || !(o instanceof String)
+                || Long.parseLong((String)o) != Long.MAX_VALUE) {
+            error = "bad object long"; return false;
+        }
+        o = rs.getObject(5);
+        if (o == null || !(o instanceof Integer)
+                || ((Integer)o).intValue() != 0) {
+            error = "bad object boolean"; return false;
+        }
+        o = rs.getObject(6);
+        if (o == null || !(o instanceof Integer)
+                || ((Integer)o).byteValue() != (byte)7) {
+            error = "bad object byte"; return false;
+        }
+        o = rs.getObject(7);
+        if (o == null || !(o instanceof byte[])
+                || !same(origbytes, (byte[])o)) {
+            error = "bad object bytes"; return false;
         }
 
         rs.close();
