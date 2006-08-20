ee3f1c6ed56e2135cf33020a3791fc905d8e09d6
David Crawshaw <david@zentus.com>
2006-08-20 17:55:27 +1000

    improve basic prep test
    darcs-hash:20060820075527-0c629-c02f4a980102ae187c85d7679975af415058ca0f.gz

--- src/test/Test05.java
+++ src/test/Test05.java
@@ -10,75 +10,60 @@ public class Test05 implements Test.Case
     public boolean run() throws Exception {
         int res;
         Class.forName("org.sqlite.JDBC");
-        Connection conn = DriverManager.getConnection(
-            "jdbc:sqlite:build/test/test.db");
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:");
 
+        PreparedStatement prepCreate, prepInsert, prepSelect, prep;
         ResultSet rs;
-        PreparedStatement prepDrop = conn.prepareStatement(
-            "DROP TABLE IF EXISTS Test5;");
-        if (prepDrop.execute()) {
-            error = "expected drop to return true";
-            return false;
-        }
-        prepDrop.close();
 
-        PreparedStatement prepCreate = conn.prepareStatement( 
-            "CREATE TABLE Test5 (id);");
-        if (prepCreate.execute()) {
-            error = "expected create to return true";
-            return false;
-        }
+        prepCreate = conn.prepareStatement("create table Test5 (id);");
+        if (prepCreate.execute()) { error = "expected create"; return false; }
         prepCreate.close();
 
-
-        PreparedStatement prepInsert = conn.prepareStatement(
-            "INSERT INTO Test5 VALUES (?);");
-        PreparedStatement prepSelect = conn.prepareStatement(
-            "SELECT id FROM Test5 WHERE id = ?;");
+        prepInsert = conn.prepareStatement("insert into Test5 values (?);");
+        prepSelect = conn.prepareStatement("select id from Test5 where id=?;");
 
         prepInsert.setInt(1, 2);
         if ((res = prepInsert.executeUpdate()) != 1) {
-            error = "expected result of 1 to update, got " + res;
-            return false;
-        }
+            error = "expected res 1 to update, got " + res; return false; }
 
-        prepInsert.clearParameters();
         prepInsert.setString(1, "Hello World, this is a string.");
         if ((res = prepInsert.executeUpdate()) != 1) {
-            error = "expected result of 1 to update, got " + res;
-            return false;
-        }
+            error = "expected res 1 (b) to update, got " + res; return false; }
 
         prepInsert.clearParameters();
         prepInsert.setString(1, "Testing euro sign: \u20ac");
         if ((res = prepInsert.executeUpdate()) != 1) {
-            error = "expected result of 1 to update, got " + res;
-            return false;
-        }
+            error = "expected res 1 (c) to update, got " + res; return false; }
 
         prepInsert.clearParameters();
         prepInsert.setInt(1, 3);
         if ((res = prepInsert.executeUpdate()) != 1) {
-            error = "expected result of 1 to update, got " + res;
-            return false;
-        }
+            error = "expected res 1 (d) to update, got " + res; return false; }
         prepInsert.close();
+
 
         prepSelect.setInt(1, 2);
         rs = prepSelect.executeQuery();
         if (!rs.next() || rs.getInt(1) != 2) {
-            error = "expected result with id=2";
-            return false;
-        }
-        prepSelect.clearParameters();
+            error = "bad res with id=2"; return false; }
+        rs.close();
 
         prepSelect.setInt(1, 3);
         rs = prepSelect.executeQuery();
         if (!rs.next() || rs.getInt(1) != 3) {
-            error = "expected result with id=3";
-            return false;
-        }
+            error = "bad res with id=3"; return false; }
         prepSelect.close();
+
+
+        prep = conn.prepareStatement("select ?, ? ,?;");
+        prep.setString(1, "one");
+        prep.setString(2, "two");
+        prep.setString(3, null);
+        rs = prep.executeQuery();
+        if (!rs.next()) { error = "bad rs next"; return false; }
+        if (!"one".equals(rs.getString(1))) { error="bad rs 1"; return false; }
+        if (!"two".equals(rs.getString(2))) { error="bad rs 2"; return false; }
+        if (rs.getString(3) != null) { error = "bad rs 3"; return false; }
 
         conn.close();
         return true;
