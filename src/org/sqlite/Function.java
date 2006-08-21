11212a92caa61649750c74cdd5ebebc69a6bbb11
David Crawshaw <david@zentus.com>
2006-08-21 11:35:54 +1000

    initial documentation for UDF interface
    darcs-hash:20060821013554-0c629-33e97b49cb103fdc670b3ad33a3bc5302ca07cef.gz

--- src/org/sqlite/Function.java
+++ src/org/sqlite/Function.java
@@ -2,6 +2,38 @@ package org.sqlite;
 
 import java.sql.*;
 
+/** Provides an interface for creating SQLite user-defined functions.
+ *
+ * <p>A subclass of <tt>org.sqlite.Function</tt> can be registered with
+ * <tt>Function.create()</tt> and called by the name it was given. All
+ * functions must implement <tt>xFunc()</tt>, which is called when SQLite
+ * runs the custom function.</p>
+ *
+ * Eg.
+ *
+ * <pre>
+ *      Class.forName("org.sqlite.JDBC");
+ *      Connection conn = DriverManager.getConnection("jdbc:sqlite:");
+ *
+ *      Function.create(conn, "myFunc", new Function() {
+ *          protected synchronized void xFunc() {
+ *              System.out.println("myFunc called!");
+ *          }
+ *      });
+ *
+ *      conn.createStatement().execute("select myFunc();");
+ *  </pre>
+ *
+ *  <p>Arguments passed to a custom function can be accessed using the
+ *  <tt>protected</tt> functions provided. <tt>args()</tt> returns
+ *  the number of arguments passed, while
+ *  <tt>value_&lt;type&gt;(int)</tt> returns the value of the specific
+ *  argument. Similarly a function can return a value using the
+ *  <tt>result(&lt;type&gt;)</tt> function.</p>
+ *
+ *  <p>Aggregate functions are not yet supported, but coming soon.</p>
+ *
+ */
 public abstract class Function
 {
     private Conn conn;
@@ -11,6 +43,8 @@ public abstract class Function
     long value = 0;       // pointer sqlite3_value*, first of 'args' array
     int args = 0;
 
+    /** Registers the given function with the Connection using the
+     *  provided name. */
     public static final void create(Connection conn, String name, Function f)
             throws SQLException {
         if (conn == null || !(conn instanceof Conn))
@@ -24,6 +58,7 @@ public abstract class Function
         f.db.create_function(name, f);
     }
 
+    /** Removes the named function form the Connection. */
     public static final void destroy(Connection conn, String name)
             throws SQLException {
         if (conn == null || !(conn instanceof Conn))
@@ -32,41 +67,71 @@ public abstract class Function
     }
 
 
+    /** Called by the JDBC driver when the custom function is called
+     *  by SQLite. */
     protected abstract void xFunc() throws SQLException;
 
 
+    /** Returns the number of arguments passed to the function.
+     *  Can only be called from <tt>xFunc()</tt>. */
     protected synchronized final int args() { return args; }
 
-    protected synchronized final void result(byte[] val) throws SQLException {
-        db.result_blob(context, val); }
-    protected synchronized final void result(double v) throws SQLException {
-        db.result_double(context, v); }
+    /** Called by <tt>xFunc</tt> to return a value. */
+    protected synchronized final void result(byte[] value) throws SQLException {
+        db.result_blob(context, value); }
+
+    /** Called by <tt>xFunc</tt> to return a value. */
+    protected synchronized final void result(double value) throws SQLException {
+        db.result_double(context, value); }
+
+    /** Called by <tt>xFunc</tt> to return a value. */
     protected synchronized final void result(int value) throws SQLException {
         db.result_int(context, value); }
-    protected synchronized final void result(long val) throws SQLException {
-        db.result_long(context, val); }
+
+    /** Called by <tt>xFunc</tt> to return a value. */
+    protected synchronized final void result(long value) throws SQLException {
+        db.result_long(context, value); }
+
+    /** Called by <tt>xFunc</tt> to return a value. */
     protected synchronized final void result() throws SQLException {
         db.result_null(context); }
-    protected synchronized final void result(String val) throws SQLException {
-        db.result_text(context, val); }
 
+    /** Called by <tt>xFunc</tt> to return a value. */
+    protected synchronized final void result(String value) throws SQLException {
+        db.result_text(context, value); }
+
+    /** Called by <tt>xFunc</tt> to throw an error. */
     protected synchronized final void error(String er) throws SQLException {
         db.result_error(context, er); }
 
+    /** Called by <tt>xFunc</tt> to access the value of an argument. */
     protected synchronized final int value_bytes(int arg) throws SQLException {
         checkValue(arg); return db.value_bytes(this, arg); }
+
+    /** Called by <tt>xFunc</tt> to access the value of an argument. */
     protected synchronized final String value_text(int arg) throws SQLException{
         checkValue(arg); return db.value_text(this, arg); }
+
+    /** Called by <tt>xFunc</tt> to access the value of an argument. */
     protected synchronized final byte[] value_blob(int a) throws SQLException {
         checkValue(a); return db.value_blob(this, a); }
+
+    /** Called by <tt>xFunc</tt> to access the value of an argument. */
     protected synchronized final double value_double(int a) throws SQLException{
         checkValue(a); return db.value_double(this, a); }
+
+    /** Called by <tt>xFunc</tt> to access the value of an argument. */
     protected synchronized final int value_int(int arg) throws SQLException {
         checkValue(arg); return db.value_int(this, arg); }
+
+    /** Called by <tt>xFunc</tt> to access the value of an argument. */
     protected synchronized final long value_long(int arg) throws SQLException {
         checkValue(arg); return db.value_long(this, arg); }
+
+    /** Called by <tt>xFunc</tt> to access the value of an argument. */
     protected synchronized final int value_type(int arg) throws SQLException {
         checkValue(arg); return db.value_type(this, arg); }
+
 
     private void checkValue(int arg) throws SQLException {
         if (conn == null || conn.isClosed() || value == 0)
